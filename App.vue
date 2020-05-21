263798fc38d65a523070b851ff2b6603c4c52d97
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2020-05-21 20:04:17 +0800

    tiger 5-21

--- App.vue
+++ App.vue
@@ -13,5 +13,80 @@
 </script>
 
 <style>
-	/*每个页面公共css */
+	/* 页面容器 */
+	page {
+		height: 100%;
+	}
+	
+	.container {
+		width: 750rpx;
+		height: auto;
+		font-family: PingFang-SC-Regular;
+		font-weight: 400;
+		overflow: auto;
+	}
+	
+	.w100 {
+		width: 100%;
+	}
+	
+	.h100 {
+		height: 100%;
+	}
+	
+	.wh {
+		width: 100%;
+		height: 100%;
+	}
+	
+	/* 定位 */
+	.p-r {
+		position: relative;
+	}
+	
+	.p-a {
+		position: absolute;
+	}
+	
+	.p-f {
+		position: fixed;
+	}
+	
+	/* 弹性布局 */
+	.flex {
+		display: flex;
+	}
+	
+	.col {
+		flex-direction: column;
+	}
+	
+	.fl-w {
+		flex-wrap: wrap;
+	}
+	
+	.fl-align {
+		align-items: center;
+	}
+	
+	.fl-jc {
+		justify-content: center;
+	}
+	
+	.fl-jsb {
+		justify-content: space-between;
+	}
+	
+	.fl-jsa {
+		justify-content: space-around;
+	}
+	
+	.fl-jse {
+		justify-content: space-evenly;
+	}
+	
+	.fl-jsd {
+		justify-content: flex-end;
+	}
+	
 </style>
